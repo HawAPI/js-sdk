6822dd2afdf1d7b6a218b45a818f9be86767039f
Lucas Josino <contact@lucasjosino.com>
2023-07-09 18:26:25 -0300

    feat: Add constants file

---
+++ src/Constants.ts
@@ -0,0 +1,14 @@
+/**
+ * Default API url
+ */
+export const API_URL = 'https://hawapi.theproject.id/api';
+
+/**
+ * Default API version
+ */
+export const API_VERSION = 'v1';
+
+/**
+ * Default API language
+ */
+export const API_DEFAULT_LANG = 'en-US';
