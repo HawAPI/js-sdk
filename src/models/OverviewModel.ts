fe194785fb50286f05a447f6e6c43cf20eeff737
Lucas Josino <contact@lucasjosino.com>
2023-11-07 20:44:36 -0300

    feat: Add `data_count` to overview model

--- src/models/OverviewModel.ts
+++ src/models/OverviewModel.ts
@@ -1,9 +1,20 @@
 import { BaseModel } from './base/BaseModel';
 
+interface DataCount {
+  actors: number;
+  characters: number;
+  episodes: number;
+  games: number;
+  locations: number;
+  seasons: number;
+  soundtracks: number;
+}
+
 export interface OverviewModel extends BaseModel {
   title: string;
   description: string;
   language: string;
   languages: string;
   creators: string;
+  data_count: DataCount;
 }
